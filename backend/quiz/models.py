0e5f5fd26f0b2c87d9d86caf7651738a2b676102
jihyekwak <jihyekwak53@gmail.com>
2022-04-17 00:37:09 -0700

    created models.py and migrate

--- backend/quiz/models.py
+++ backend/quiz/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Subject(models.Model):
+    name = models.CharField(max_length=50)
+
+DIFFICULTY_CHOICES = {
+    ('easy', 'easy'),
+    ('medium', 'medium'),
+    ('difficult', 'difficult')
+}
+
+GRADE_CHOICES = {
+    ('Pre-k', 'Pre-K'),
+    ('Kindergarten', 'Kindergarten'),
+    ('1st Grage', '1st Grade'),
+    ('2nd Grage', '2nd Grade'),
+    ('3rd Grage', '3rd Grade'),
+}
+
+class Quiz(models.Model):
+    text = models.CharField(max_length=250)
+    subject = models.ForeignKey(Subject, related_name='quizzes', on_delete=models.CASCADE)
+    difficulty = models.CharField(max_length=20, choices=DIFFICULTY_CHOICES, blank = True)
+    grade = models.CharField(max_length=50, choices=GRADE_CHOICES, blank = True)
+
+class Question(models.Model):
+    text = models.CharField(max_length=250)
+
+class Answer(models.Model):
+    text = models.CharField(max_length=250)
+    question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
+    is_correct = models.BooleanField(default=False)
