6f5bdf25e997519697cad3030de2916ed6f5edf6
jihyekwak <jihyekwak53@gmail.com>
2022-04-17 21:19:05 -0700

    update admin.py

--- backend/quiz/admin.py
+++ backend/quiz/admin.py
@@ -3,7 +3,47 @@ from .models import Subject, Quiz, Question, Answer
 
 # Register your models here.
 
-admin.site.register(Subject)
-admin.site.register(Quiz)
-admin.site.register(Question)
-admin.site.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['question', 'text', 'is_correct']
+    list_display_links = ['question']
+    list_editable = ['text', 'is_correct']
+    list_filter = ['question']
+
+admin.site.register(Answer, AnswerAdmin)
+
+class AnswerAdminInline(admin.TabularInline):
+    model = Answer
+    fk_name = "question"
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerAdminInline]
+    list_display = ['quiz', 'text']
+    list_display_links = ['quiz']
+    list_editable = ['text']
+    list_filter = ['quiz']
+
+admin.site.register(Question, QuestionAdmin)
+
+class QuestionAdminInline(admin.TabularInline):
+    model = Question
+    fk_name = "quiz"
+
+class QuizAdmin(admin.ModelAdmin):
+    inlines = [QuestionAdminInline]
+
+    list_display = ['title', 'subject', 'difficulty', 'grade', 'created_at']
+    list_display_links = ['title']
+
+admin.site.register(Quiz, QuizAdmin)
+
+class QuizAdminInline(admin.TabularInline):
+    model = Quiz
+    fk_name = "subject"
+
+class SubjectAdmin(admin.ModelAdmin):
+    inlines = [QuizAdminInline]
+
+    list_display = ['name']
+    list_display_links = ['name']
+
+admin.site.register(Subject, SubjectAdmin)
