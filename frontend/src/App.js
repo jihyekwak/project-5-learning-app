19476d91b259c44ed9d5dea5bc248c2c6a7046f1
jihyekwak <jihyekwak53@gmail.com>
2022-04-17 23:24:59 -0700

    test backend api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+  const [quizList, setQuizList] = useState([])
+
+  useEffect(() => {
+    axios
+    .get("/api/quizzes/")
+    .then((res)=> {
+      console.log(res.data)
+      setQuizList(res.data)
+    })
+    .catch((err) => console.log(err))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Learning App</h1>
+      <h2>Quiz List</h2>
+        {quizList.map((quiz) => {
+          return (
+            <div 
+              style={{
+                border: '2px solid gray',
+                width: '450px',
+                height: '200px',
+                borderRadius: '20px',
+                display: 'inline-block',
+                margin: '20px'
+              }}
+              key={quiz.id}>
+              <>
+                <h2>{quiz.title}</h2>
+                <p>{quiz.subject}</p>
+                <p>{quiz.grade}</p>
+                <p>{quiz.difficulty}</p>
+              </>
+            </div>
+          )
+        })}
     </div>
   );
 }
