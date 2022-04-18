e6b16ac884ca0bdb6ae154150165f3080710d743
jihyekwak <jihyekwak53@gmail.com>
2022-04-18 00:13:54 -0700

    install react-router-dom, setup Navbar

--- frontend/src/components/NavBar/index.jsx
+++ frontend/src/components/NavBar/index.jsx
@@ -1,8 +1,12 @@
+import {NavLink} from 'react-router-dom';
+
 const NavBar = () => {
 
     return(
         <div>
-
+            <NavLink to='/'>Logo</NavLink>
+            <NavLink to='/'>Main</NavLink>
+            <NavLink to='/'>MyPage</NavLink>
         </div>
     )
 }
