6f8e4c207d33ffff2a2c797ec2740ee751714f96
jihyekwak <jihyekwak53@gmail.com>
2022-04-25 10:36:09 -0700

    add profile component to mypage

--- frontend/src/pages/MyPage/index.js
+++ frontend/src/pages/MyPage/index.js
@@ -1,19 +1,29 @@
-import { Container } from "@material-ui/core";
+import { Container, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import QuizForm from "../../components/QuizForm";
 import QuizListTable from "../../components/QuizListTable";
+import Profile from "../../components/Profile";
 
 const useStyles = makeStyles((theme) => ({
 
 }))
 
-const MyPage = () => {
+const MyPage = ({profile}) => {
 
     const classes = useStyles();
 
     return(
         <Container>
-            <QuizForm />
+            <Grid container>
+                <Grid item xs={2}>
+                    <Profile profile={profile}/>
+                </Grid>
+                <Grid item xs={10}>
+                    <QuizForm />
+                </Grid>
+            </Grid>
+
+
             {/* <QuizListTable /> */}
         </Container>
     )
